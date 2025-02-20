75fcfcdbeb5d3a4d595961bbce6922e956786402
id5090261 <id5090261@gmail.com>
2025-02-20 18:09:09 +0330

    Add tabs

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-document.getElementById("DarkModeToggle").addEventListener("click", () => {
-    document.querySelector("body").classList.toggle("white");
-    document.querySelectorAll("svg").forEach(svg => {
-        svg.classList.toggle("white");
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("DarkModeToggle").addEventListener("click", () => {
+        document.querySelector("body").classList.toggle("white");
+        document.querySelectorAll("svg").forEach(svg => {
+            svg.classList.toggle("white");
+        });
+    });
+
+    const tabs = document.querySelectorAll(".tab");
+    const contents = document.querySelectorAll(".content");
+    tabs.forEach((tab, index) => {
+        tab.addEventListener("click", () => {
+            document.querySelector(".tab-content").style.display = "block";
+            contents.forEach(content => content.style.display = "none");
+            contents[index].style.display = "block";
+        });
     });
 });
